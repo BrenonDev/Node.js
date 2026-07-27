fe082cabd10b151330531d310bf833fa18bc1daf
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-27 15:31:29 -0300

    initial commit

---
+++ 1 - Iniciando com Node.js/scripts.js
@@ -0,0 +1,20 @@
+console.log("=== CONHECENDO O NODE.JS ===")
+
+// O Node.js não é uma linguagem de programação. Javascript é a linguagem de programação que o Node é capaz de executar.
+
+// O Node.js foi criado por Ryan Dahl em 2009 no decorrer de uma análise de como as páginas da web funcionavam.
+// Se os Browsers conseguem compreender Javascript, como podemos utilizar esse mecanismo em uma plataforma que entenda Javascript para outros propósitos?
+
+// O Node.js é baseado na V8 JavaScript Engine do Google Chrome, que é um interpretador JavaScript (o coração que processa todo o código Javascript para a máquina compreender fora do navegador).
+// Desenvolvido pela Google e utilizado em seu navegador Google Chrome. O v8 foi desenvolvido em C++ com o objetivo de aumentar a performance de execução do Javascript.
+
+// Javascript para diferentes propósitos:
+// Node.js => Back end
+// React => Front Web
+// React Native => Mobile
+// Electron => Desktop
+// Etc.
+
+
+
+// ======================================================================
