47c8c2fb38246fd86ce34e537207543e828cd633
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-29 14:21:31 -0300

    o que é uma API

---
+++ 2 - Fundamentos de API/scripts.js
@@ -0,0 +1,27 @@
+console.log("=== O QUE É UMA API ===");
+
+// API
+// Application Programming Interface (Interface de Programação de Aplicação) oferece funcionalidades sem a necessidade de saber como foram implementados.
+
+//                              Requisição              Requisição
+//  [         Client         ]    ----->    [  API   ]    ----->    [  Server ]
+//  [(Cliente do restaurante)]    <-----    [(Garçom)]    <-----    [(Cozinha)]
+//                               Resposta                Resposta
+
+// Rota
+
+// Uma rota é um ponto de extremidade que associa uma URL específica a uma função que manipula requisições HTTP.
+
+// Em outras palavras, uma rota é o caminho pelo qual o servidor responde a requisições HTTP.
+
+//  |--------------------------------------------------|
+//  |                                                  |
+//  |          https://meuservidor.com.br/products     |
+//  |           ^              ^              ^        |
+//  |           |              |              |        |
+//  |     protocolo | endereço do servidor | rota      |
+//  |                                                  |
+//  |--------------padrão de comunicação---------------|
+
+
+// ======================================================================
