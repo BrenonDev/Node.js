e1308c7a7f971263efb5222b58dba1496a6b06bb
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-08-06 13:41:13 -0300

    armazenando dados

---
+++ 3 - Manipulando Dados/scripts.js
@@ -0,0 +1,10 @@
+console.log("=== ARMAZENANDO DADOS ===");
+
+//  ESTRATÉGIA PARA SALVAR DADOS
+
+// in-memory => Memória do programa (variáveis, arrays, objetos), volátil, não persiste após o encerramento do programa
+// arquivo (JSON) => Persistência em disco, não volátil, pode ser lido e escrito, mas não é eficiente para grandes volumes de dados
+// database => Persistência em disco, não volátil, pode ser lido e escrito, eficiente para grandes volumes de dados, mas requer configuração e manutenção
+
+
+// ======================================================================
